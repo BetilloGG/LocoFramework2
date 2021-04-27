6d98f4368dee34dc4bd20c48caf3156ad905197f
Alberto Gonzalez <jgonzalez@ia.com.mx>
2021-04-26 22:36:49 -0500

    Init our commit

---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by José Alberto González Gordillo on 26/04/21.
+//
+
+import Foundation
+
+public class Service {
+    private init() { }
+
+    public static func doSomeThings() -> String {
+        return "Welcome to doSomeThings"
+    }
+}
